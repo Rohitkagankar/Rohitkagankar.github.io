086a91762d3bb0ba68a1efe9aa48c296dc268422
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-01-18 13:46:23 +0530

    home page

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Python Fullstack Developer","Web Designer","AI engineer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDeley:1000,
+    loop:true
+})
