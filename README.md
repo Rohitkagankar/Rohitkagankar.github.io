295e71e52d0386b1ada6ad490a5feed663bc93b0
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-07-30 23:21:26 +0530

    css and structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Rohitkagankar.github.io
+This is my Portfolio.
+https://github.com/Rohitkagankar
--- main.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text",{
-    strings:["Python Fullstack Developer","Web Designer","AI engineer"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDeley:1000,
-    loop:true
-})
