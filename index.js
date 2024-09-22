758ef880f4e741980801ea0ee4b60e25c8617fd9
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-09-22 10:09:23 +0530

    project added

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ window.onscroll = () => {
 
 document.getElementById('resume-button-1').addEventListener("click", () => {
   window.open(
-    "https://drive.google.com/file/d/1g7MpiQI0DGAvSzAeLQEEAStxVAB6DWTb/view?usp=drive_link",
+    "https://drive.google.com/file/d/1iKHf4AzxXTgaDvLjO0YZ7TF4MBxdgMlx/view?usp=sharing",
     "_blank"
   );
 });
 
 document.getElementById('resume-button-2').addEventListener("click", () => {
   window.open(
-    "https://drive.google.com/file/d/1g7MpiQI0DGAvSzAeLQEEAStxVAB6DWTb/view?usp=drive_link",
+    "https://drive.google.com/file/d/1iKHf4AzxXTgaDvLjO0YZ7TF4MBxdgMlx/view?usp=sharing",
     "_blank"
   );
 });
